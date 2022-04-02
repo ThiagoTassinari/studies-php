18801a37fe275bd07520ee71e0728d8d664519ed
ThiagoTassinari <thiagotassinari12@gmail.com>
2022-04-02 20:17:02 -0300

    I did the basic math operations

---
+++ study/operations.php
@@ -0,0 +1,21 @@
+<?php
+
+// Operações básicas para cálculos matemáticos
+$soma = 2 + 2;
+echo "A resposta do adiação é: $soma" . PHP_EOL;
+
+$subtracao = 2 - 2;
+echo "A resposta da subtração é: $subtracao" . PHP_EOL;
+
+$multiplicacao = 2 * 2;
+echo "A resposta da multiplicação é: $multiplicacao" . PHP_EOL;
+
+$divisao = 2 / 2;
+echo "A resposta da divisão é: $divisao" . PHP_EOL;
+
+$doisAoCubo = 2 ** 3;
+echo "$A resposta da potência ao quadrado é: $doisAoCubo" . PHP_EOL;
+
+$restoDaDivisao = 10 % 3;
+echo "A resposta do resto da divisão é: $restoDaDivisao" . PHP_EOL;
+
