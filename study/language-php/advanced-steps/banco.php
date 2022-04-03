964c59e0ed2c2a11ce8e30c9faa0e95746d5a9f7
ThiagoTassinari <thiagotassinari12@gmail.com>
2022-04-03 01:57:56 -0300

    I created the deposit function and typed the variables

--- study/language-php/advanced-steps/banco.php
+++ study/language-php/advanced-steps/banco.php
@@ -1,7 +1,8 @@
 <?php
 
 // Função: É algo que retorna um valor após sua execução
-function sacar($conta, $valorASacar) {
+function sacar(array $conta, float $valorASacar): array 
+{
     if ($valorASacar > $conta['saldo']) {
         exibiMessagem('Você não pode sacar');
     } else {
@@ -11,8 +12,20 @@ function sacar($conta, $valorASacar) {
     return $conta;
 }
 
+function depositar(array $conta, float $valorADepositar): array 
+{
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibiMessagem('Depósitos precisam ter valores positivos');
+    }
+
+    return $conta;
+}
+
 // Subrotina: É uma função que não retorna valor na sua execução.
-function exibiMessagem($messagem) {
+function exibiMessagem(string $messagem) 
+{
     echo $messagem . PHP_EOL;
 };
 
@@ -33,6 +46,8 @@ $contasCorrentes = [
 
 $contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'], 500);
 $contasCorrentes['123.456.489-11'] = sacar($contasCorrentes['123.456.489-11'], 500);
+
+$contasCorrentes['123.256.781-11'] = depositar($contasCorrentes['123.256.781-11'], -100);
 
 foreach ($contasCorrentes as $cpf => $conta) {
     exibiMessagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
