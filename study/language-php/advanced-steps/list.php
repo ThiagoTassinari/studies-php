141d71daf48849b1f8c28fab3a7fb83429b64d11
ThiagoTassinari <thiagotassinari12@gmail.com>
2022-04-02 22:42:22 -0300

    Ways to declare an array

---
+++ study/language-php/advanced-steps/list.php
@@ -0,0 +1,15 @@
+<?php
+
+// Declarações Array - Primeiro Modo
+$idadeList = [21, 23, 19, 25, 30, 41, 18];
+
+$indexList = $idadeList[3];
+echo "The index of this list is: $indexList" . PHP_EOL;
+
+/* ------------------------------------------------- */
+
+// Declarações Array - Segundo Modo
+$shopList = array("Apple", "Orange", "Grapes", "Meet", "Chicken", "Potatos", "Tomatos");
+
+$itemList = $shopList[3];
+echo "The item of this list is: $itemList" . PHP_EOL;
